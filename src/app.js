b80281a023df44c81788524e94d31019e146722c
codebender <lionelmbankeu@gmail.com>
2021-02-13 15:26:45 +0200

    why so bad

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-        <div>Hello China</div>
+        <div>Hello Chinas</div>
     </div>
   );
 }
